3286469e35a06f0730879b6b9e9d1e94a079febb
Tilir <konstantin.vladimirov@gmail.com>
2019-04-12 20:15:32 +0300

    More on control graph

--- include/controlgraph/controlgraph.h
+++ include/controlgraph/controlgraph.h
@@ -2,6 +2,39 @@
 //
 // Control-flow (top-level, before locations) for given functions
 //
+// control graph itself is array of split trees, one for each call node
+// plus some public interface
+//
+// split tree details left for implementation
+//
+// Node types:
+//
+// 1. Loops
+//
+// LOOP -> [ BODY ]
+//
+// it is something like for, do-while, while, etc...
+//
+// 2. Conditional operands
+//
+// IF -> BRANCHING -> [ BODY ]
+//    -> BRANCHING -> [ BODY ]
+//    -> BRANCHING -> [ BODY ]
+//
+// SWITCH -> BRANCHING -> [ BODY ]
+//        -> BRANCHING -> [ BODY ]
+//        -> BRANCHING -> [ BODY ]
+//
+// it is something like if-elseif-else or switch case-case-default
+//
+// 3. Irreducible regions
+//
+// REGION -> BRANCHING -> [ BODY ]
+//        -> BRANCHING -> [ BODY ]
+//        -> BRANCHING -> [ BODY ]
+//
+// 4. BLOCKs, function CALLs, etc
+//
 //------------------------------------------------------------------------------
 //
 // This file is licensed after LGPL v3
@@ -12,7 +45,12 @@
 #pragma once
 
 #include <iostream>
+#include <list>
 #include <memory>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
 
 #include "config/configs.h"
 
@@ -30,11 +68,89 @@ class varassign_t;
 
 namespace cn {
 
+using vertex_t = int;
+constexpr vertex_t ILLEGAL_VERTEX = -1;
+
+enum class category_t {
+  ILLEGAL = -1,
+  BLOCK = 0,
+  CALL,
+  LOOP,
+  IF,
+  SWITCH,
+  REGION,
+  BRANCHING,
+  CATMAX
+};
+
+struct block_t {
+  static constexpr category_t cat = category_t::BLOCK;
+};
+
+enum class call_type_t { DIRECT, CONDITIONAL, INDIRECT };
+
+struct call_t {
+  static constexpr category_t cat = category_t::CALL;
+  call_type_t type;
+  int nfunc;
+};
+
+struct loop_t {
+  static constexpr category_t cat = category_t::LOOP;
+  int start, stop, step;
+};
+
+struct if_t {
+  static constexpr category_t cat = category_t::IF;
+};
+
+struct switch_t {
+  static constexpr category_t cat = category_t::SWITCH;
+};
+
+struct region_t {
+  static constexpr category_t cat = category_t::REGION;
+};
+
+struct branching_t {
+  static constexpr category_t cat = category_t::BRANCHING;
+};
+
+using common_t = std::variant<block_t, call_t, loop_t, if_t, switch_t, region_t,
+                              branching_t>;
+
+struct vertexprop_t {
+  vertex_t id = ILLEGAL_VERTEX;
+  category_t cat = category_t::ILLEGAL;
+  common_t type;
+
+  vertexprop_t() = default;
+  explicit vertexprop_t(vertex_t i, category_t c, common_t t)
+      : id(i), cat(c), type(t) {}
+};
+
+template <typename T, typename... Ts>
+vertexprop_t create_vprop(vertex_t id, Ts &&... args) {
+  return vertexprop_t{id, T::cat, T{std::forward<Ts>(args)...}};
+}
+
+class split_tree_t;
+
+struct split_tree_deleter_t {
+  void operator()(split_tree_t *);
+};
+
+using stt = std::unique_ptr<split_tree_t, split_tree_deleter_t>;
+
+using vertex_iter_t = typename std::list<vertex_t>::const_iterator;
+
 class controlgraph_t final {
   cfg::config config_;
   std::shared_ptr<tg::typegraph_t> tgraph_;
   std::shared_ptr<cg::callgraph_t> cgraph_;
   std::shared_ptr<va::varassign_t> vassign_;
+
+  std::vector<stt> strees_;
 
   // public interface
 public:
@@ -42,7 +158,19 @@ public:
                           std::shared_ptr<cg::callgraph_t>,
                           std::shared_ptr<va::varassign_t>);
 
-  void dump(std::ostream &os);
+  int nfuncs() const { return cgraph_->nfuncs(); }
+
+  // top-level iterator by function number
+  vertex_iter_t begin(int nfunc) const;
+  vertex_iter_t end(int nfunc) const;
+
+  // childs iterator
+  vertex_iter_t begin_childs(int nfunc, vertex_t parent) const;
+  vertex_iter_t end_childs(int nfunc, vertex_t parent) const;
+
+  vertexprop_t from_vertex(int nfunc, vertex_t) const;
+
+  void dump(std::ostream &os) const;
 };
 
 } // namespace cn
--- lib/controlgraph/controlgraph.cc
+++ lib/controlgraph/controlgraph.cc
@@ -2,6 +2,19 @@
 //
 // Control-flow (top-level, before locations) for given functions
 //
+// split tree is slightly tricky
+// it have labeled childs (order of childs in node is important)
+// so boost graph is bad decision, and thus it is modeled by:
+// (1) child list for each vertex
+// (2) unordered map from vertex to its parent
+// (3) unordered map from vertex to its bundled description
+// (4) unordered set of vertices which are bbs, available to split
+//
+// Then typical split is trivial:
+// * we are choosing random block from (3)
+// * we are selecting its parent from (2)
+// * we are making split in parents adjlist
+//
 //------------------------------------------------------------------------------
 //
 // This file is licensed after LGPL v3
@@ -9,13 +22,66 @@
 //
 //------------------------------------------------------------------------------
 
-#include "controlgraph.h"
+#include <iostream>
+#include <list>
+#include <memory>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
 #include "../dbgstream.h"
 #include "callgraph/callgraph.h"
+#include "controlgraph.h"
 #include "typegraph/typegraph.h"
 #include "varassign/varassign.h"
 
 namespace cn {
+
+using vct = std::vector<vertexprop_t>;
+using vcit = typename vct::iterator;
+
+class split_tree_t {
+  cfg::config cf_;
+  std::list<vertex_t> toplev_;
+  std::vector<std::list<vertex_t>> adj_;
+  std::unordered_map<vertex_t, vertex_t> parent_of_;
+  std::unordered_map<vertex_t, vertexprop_t> desc_of_;
+  std::unordered_set<vertex_t> bbs_;
+
+public:
+  split_tree_t(const cfg::config &cf, vcit start, vcit fin) : cf_(cf) {
+    for (vcit cur = start; cur != fin; ++cur) {
+      toplev_.push_back(adj_.size());
+      adj_.emplace_back();
+    }
+  }
+
+  vertex_iter_t begin() const { return toplev_.begin(); }
+  vertex_iter_t end() const { return toplev_.end(); }
+
+  // childs iterator
+  vertex_iter_t begin_childs(vertex_t parent) const {
+    return adj_[parent].begin();
+  }
+  vertex_iter_t end_childs(vertex_t parent) const { return adj_[parent].end(); }
+
+  vertexprop_t from_vertex(vertex_t v) const {
+    auto vit = desc_of_.find(v);
+    if (vit == desc_of_.end())
+      throw std::runtime_error("Vertex not found");
+    return vit->second;
+  }
+
+  // TODO: tree-like print
+  void dump(std::ostream &os) const {
+    for (auto tl : toplev_) {
+      os << tl << "\n";
+    }
+  }
+};
+
+void split_tree_deleter_t::operator()(split_tree_t *pst) { delete pst; }
 
 //------------------------------------------------------------------------------
 //
@@ -31,9 +97,56 @@ controlgraph_t::controlgraph_t(cfg::config &&cf,
       vassign_(vassign) {
   if (!config_.quiet())
     dbgs() << "Creating controlgraph\n";
+
+  // for every call-graph function
+  for (auto cgv : *cgraph_) {
+    assert(int(cgv) < cgraph_->nfuncs());
+    vct seeds;
+    vertex_t id = 0;
+
+    seeds.emplace_back(create_vprop<block_t>(id++));
+
+    // initial seeds are direct calls
+    for (auto cit = cgraph_->callees_begin(cgv, cg::calltype_t::DIRECT);
+         cit != cgraph_->callees_end(cgv, cg::calltype_t::DIRECT); ++cit) {
+      seeds.emplace_back(
+          create_vprop<call_t>(id++, call_type_t::DIRECT, int(*cit)));
+      seeds.emplace_back(create_vprop<block_t>(id++));
+    }
+
+    assert((id > 0) && (id == int(seeds.size())));
+
+    // can not use make_unique here
+    auto st = stt{new split_tree_t{config_, seeds.begin(), seeds.end()}};
+    strees_.emplace_back(std::move(st));
+  }
 }
 
-void controlgraph_t::dump(std::ostream &) {}
+vertex_iter_t controlgraph_t::begin(int nfunc) const {
+  return strees_[nfunc]->begin();
+}
+
+vertex_iter_t controlgraph_t::end(int nfunc) const {
+  return strees_[nfunc]->end();
+}
+
+vertex_iter_t controlgraph_t::begin_childs(int nfunc, vertex_t parent) const {
+  return strees_[nfunc]->begin_childs(parent);
+}
+
+vertex_iter_t controlgraph_t::end_childs(int nfunc, vertex_t parent) const {
+  return strees_[nfunc]->end_childs(parent);
+}
+
+vertexprop_t controlgraph_t::from_vertex(int nfunc, vertex_t v) const {
+  return strees_[nfunc]->from_vertex(v);
+}
+
+void controlgraph_t::dump(std::ostream &os) const {
+  os << "Controlgraph consists of " << nfuncs() << " functions\n";
+  for (auto &&t : strees_)
+    t->dump(os);
+}
 
 } // namespace cn
 
