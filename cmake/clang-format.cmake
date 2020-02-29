dfbd15e0992c0e30aa6abe7a7e249bb1b42485d7
Alexander Us <coillol@yandex.ru>
2020-02-29 18:39:23 +0300

    [Build] Add option to control clang-format dependency for targets
    AUTO_CLANG_FORMAT=ON/OFF -- enable/disable automatic clang-format
    run for sources when building. Defaults to ON.

--- cmake/clang-format.cmake
+++ cmake/clang-format.cmake
@@ -1,15 +1,22 @@
-find_program(CLANG_FORMAT clang-format HINTS ${CLANG_FORMAT_PATH})
+option(AUTO_CLANG_FORMAT "Enable automatic run of clang-format on every build" ON)
 
-if (NOT CLANG_FORMAT)
-  message(STATUS "Probably need to specify CLANG_FORMAT_PATH")
-  message(FATAL_ERROR "Clang-format not found")
+if (AUTO_CLANG_FORMAT)
+  find_program(CLANG_FORMAT clang-format HINTS ${CLANG_FORMAT_PATH})
+
+  if (NOT CLANG_FORMAT)
+    message(STATUS "Probably need to specify CLANG_FORMAT_PATH")
+    message(FATAL_ERROR "Clang-format not found")
+  endif()
+
+  file(GLOB_RECURSE ALL_SOURCE_FILES *.cpp *.cc *.h *.hpp)
+
+  add_custom_target(
+    clangformat
+    COMMAND ${CLANG_FORMAT}
+    -i
+    ${ALL_SOURCE_FILES}
+    )
+else()
+  # Add fake target that does nothing if formatting is disabled.
+  add_custom_target(clangformat)
 endif()
-
-file(GLOB_RECURSE ALL_SOURCE_FILES *.cpp *.cc *.h *.hpp)
-
-add_custom_target(
-  clangformat
-  COMMAND ${CLANG_FORMAT}
-  -i
-  ${ALL_SOURCE_FILES}
-)
